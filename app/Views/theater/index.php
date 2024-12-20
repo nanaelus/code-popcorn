bbf78d930fa3c84b780545acc5a64480c6a7e527
nanaelus <anael.arnaud@laposte.net>
2024-12-20 15:48:17 +0100

    Feat: Visuel index theater and view movie

--- app/Views/theater/index.php
+++ app/Views/theater/index.php
@@ -5,23 +5,29 @@
                 <div class="card-header">
                     <h2>Tout nos Cinémas pour Vous</h2>
                 </div>
-                <div class="card-body">
+                <div class="card-body d-flex flex-wrap justify-content-between">
                     <?php foreach ($theaters as $theater): ?>
-                        <div class="card mb-3">
-                            <div class="col">
+                        <div class="card mb-3" style="width: 25em;">
+                            <div class="card-header">
+                                <?= $theater["name"]; ?>
+                            </div>
+                            <div class="card-body d-flex justify-content-center">
+                            <span>
                                 <a href="<?= base_url('theater/' . $theater['id']); ?>">
                                     <?php
-                                        if($theater['preview_url']) { ?>
-                                            <img style= width:300px height="300px" src="<?= base_url($theater['preview_url']) ?>">
-                                        <?php } else { ?>
-                                            <img style= width:300px height="300px" src="<?= base_url('assets/img/theater/default.jpg') ?>">
+                                    if($theater['preview_url']) { ?>
+                                        <img style= width:300px height="300px" src="<?= base_url($theater['preview_url']) ?>">
+                                    <?php } else { ?>
+                                        <img style= width:300px height="300px" src="<?= base_url('assets/img/theater/default.jpg') ?>">
                                     <?php } ?>
                                 </a>
+                            </span>
                             </div>
-                            <p><?= $theater["name"]; ?></p>
-                            <p>Adresse : <?= $theater['address']; ?> </p>
-                            <p>Tel : <?= $theater['phone']; ?></p>
-                            <p>Email : <?= $theater['email']; ?></p>
+                            <div class="card-footer">
+                                Adresse : <?= $theater['address']; ?>
+                                Tel : <?= $theater['phone']; ?>
+                                Email : <?= $theater['email']; ?>
+                            </div>
                         </div>
                     <?php endforeach ; ?>
                 </div>
