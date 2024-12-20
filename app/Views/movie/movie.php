07cb6f1078ddbe5fda6ab1fc6c609acbb23d05b1
nanaelus <anael.arnaud@laposte.net>
2024-12-20 16:17:17 +0100

    Visu index theater (#8)
    * Feat: Visuel index theater and view movie
    
    * Feat: Visuel index theater and view movie

--- app/Views/movie/movie.php
+++ app/Views/movie/movie.php
@@ -1,4 +1,3 @@
-<div class="container">
     <div class="row">
         <div class="col">
            <div class="card">
@@ -6,29 +5,47 @@
                    <h3><?= $movie['title']; ?></h3>
                </div>
                <div class="card-body">
-                   <div class="d-flex justify-content-between">
-                       <span>Date de Sortie : <?= $movie['release']; ?></span>
-                       <span>Durée : <?= $movie['duration']; ?> minutes</span>
+                   <div class="row">
+                       <div class="col-md-4">
+                           <div class="card h-100 text-center">
+                               <?php
+                               $img_src = !empty($movie['preview_url']) ? $movie['preview_url'] : base_url('assets/img/preview/flim.jpg');
+                               ?>
+                                <img style= width:300px height="400px" src="<?= $img_src ?>" class="card-img-top mx-auto d-block mt-2 mb-2" alt="<?= $movie['title'] ?>">
+                           </div>
+                       </div>
+                       <div class="col-md-8">
+                           <div class="card h-100">
+                               <div class="row mt-2 h-25">
+                                   <div class="col ms-2">
+                                       Date de sortie : <?= $movie['release']; ?>
+                                   </div>
+                                   <div class="col">
+                                       Durée : <?= $movie['duration']; ?> minutes
+                                   </div>
+                               </div>
+                               <div class="row h-50">
+                                   <div class="col ms-2">
+                                   Synopsis :
+                                       <p class="ms-4"><?= $movie['description']; ?></p>
+                                   </div>
+                               </div>
+                               <div class="row h-25">
+                                   <div class="col ms-2">
+                                       Genre :
+                                       <?php foreach($categories as $c) { ?>
+                                           <div class="ms-3"><?= $c['name']; ?></div>
+                                       <?php } ?>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
                    </div>
-                   <?php
-                   $img_src = !empty($movie['preview_url']) ? $movie['preview_url'] : base_url('assets/img/preview/flim.jpg');
-                   ?>
-                       <img style= width:300px height="400px" src="<?= $img_src ?>" class="card-img-top" alt="<?= $movie['title'] ?>">
-                   <div class="mb-3"><?= $movie['description']; ?></div>
-                   <div class="mb-3">
-                       Genre :
-                       <?php foreach($categories as $c) { ?>
-                           <div class="ms-3"><?= $c['name']; ?></div>
-                       <?php } ?>
-                   </div>
-                   <?php if($movie['rating'] != 'Tous Publics') { ?>
-                       Film Interdit aux Moins de : <?= $movie['rating']; ?>.
-                   <?php } ?>
                </div>
-               <div class="card-footer d-flex flex-wrap justify-content-between">
                    <?php
-                   if($showings) {
-                       foreach($showings as $showing) { ?>
+                   if($showings) { ?>
+                       <div class="card-footer d-flex flex-wrap justify-content-between">
+                       <?php foreach($showings as $showing) { ?>
                            <div class="mb-2">
                                <div class="card" style="width: 25em;">
                                     <?php $date = strtotime($showing['date']); ?>
@@ -44,10 +61,10 @@
                                </div>
                            </div>
                        <?php }
-                   }
-                   ?>
+                   } else { ?>
+                       <div class="card-footer h4 text-center">Pas de Séance Prévue</div>
+                   <?php } ?>
                </div>
            </div>
         </div>
     </div>
-</div>
