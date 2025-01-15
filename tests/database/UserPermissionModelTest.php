ff8c03cbf6fd21fb9e22979ddaeb8838e909d484
nanaelus <anael.arnaud@laposte.net>
2025-01-15 22:46:00 +0100

    Feat: refactor UserPermissionModelTest's mock data

--- tests/database/UserPermissionModelTest.php
+++ tests/database/UserPermissionModelTest.php
@@ -23,6 +23,25 @@ class UserPermissionModelTest extends CIUnitTestCase
         //truncate the table before each test
         $this->db->table('user_permission')->truncate();
 
+        $data = [
+            [
+                'id' => 1,
+                'name' => 'permitest',
+                'slug' => 'permitest',
+            ],
+            [
+                'id' => 2,
+                'name' => 'permitest2',
+                'slug' => 'permitest2',
+            ],
+            [
+                'id' => 3,
+                'name' => 'permitest3',
+                'slug' => 'permitest3',
+            ]
+        ];
+        $this->db->table('user_permission')->insertBatch($data);
+
         //re-enable foreign key checks
         $this->db->query('SET FOREIGN_KEY_CHECKS=1');
     }
@@ -42,8 +61,8 @@ class UserPermissionModelTest extends CIUnitTestCase
     public function testCreatePermission() {
         $model = new UserPermissionModel();
 
-        $data1 = ['name' => 'permitest', 'slug' => 'permitest'];
-        $data2 = ['name' => 'permitest', 'slug' => 'permitest-1'];
+        $data1 = ['name' => 'permitest666', 'slug' => 'permitest666'];
+        $data2 = ['name' => 'permitest666', 'slug' => 'permitest666-1'];
 
         $result1 = $model->createPermission($data1);
         $result2 = $model->createPermission($data2);
@@ -51,29 +70,23 @@ class UserPermissionModelTest extends CIUnitTestCase
         $this->assertTrue($result1 > 0);
         $this->assertTrue($result2 > 0);
 
-        $this->seeInDatabase('user_permission', ['name'=>'permitest', 'slug' => 'permitest']);
-        $this->seeInDatabase('user_permission', ['name'=>'permitest', 'slug' => 'permitest-1']);
+        $this->seeInDatabase('user_permission', ['name'=>'permitest666', 'slug' => 'permitest666']);
+        $this->seeInDatabase('user_permission', ['name'=>'permitest666', 'slug' => 'permitest666-1']);
     }
 
     public function testUpdatePermission() {
         $model = new UserPermissionModel();
 
-        $data = ['name' => 'permitest'];
-        $model->createPermission($data);
-
-        $updatedData = ['name' => 'permitest2'];
+        $updatedData = ['name' => 'permitest42'];
 
         $permission = $model->getUserPermissionById(1);
         $model->updatePermission($permission['id'], $updatedData);
-        $this->seeInDatabase('user_permission', ['name' => 'permitest2']);
+        $this->seeInDatabase('user_permission', ['name' => 'permitest42']);
     }
 
     public function testDeletePermission() {
         $model = new UserPermissionModel();
-        $data = ['name' => 'permitest2'];
-        $model->createPermission($data);
 
-        // Récupérer la permission que nous venons de créer
         $permission = $model->getUserPermissionById(1);
         $this->assertNotNull($permission); // Vérifiez que la permission existe avant suppression
 
@@ -86,20 +99,13 @@ class UserPermissionModelTest extends CIUnitTestCase
 
     public function testGetAllPermissions() {
         $model = new UserPermissionModel();
-        $data1 = ['name' => 'permitest'];
-        $data2 = ['name' => 'permitest2'];
-
-        $model->createPermission($data1);
-        $model->createPermission($data2);
 
         $result = $model->getAllPermissions();
-        $this->assertCount(2, $result);
+        $this->assertCount(3, $result);
     }
 
     public function testGetUserPermissionById() {
         $model = new UserPermissionModel();
-        $data = ['name' => 'permitest'];
-        $model->createPermission($data);
 
         $permission = $model->getUserPermissionById(1);
         $this->assertNotNull($permission);
@@ -109,43 +115,22 @@ class UserPermissionModelTest extends CIUnitTestCase
     public function testGetTotalPermission() {
         $model = new UserPermissionModel();
 
-        $data1 = ['name' => 'permitest'];
-        $data2 = ['name' => 'permitest2'];
-        $data3 = ['name' => 'permitest3'];
-
-        $model->createPermission($data1);
-        $model->createPermission($data2);
-        $model->createPermission($data3);
-
         $result = $model->getTotalPermission();
         $this->assertEquals(3, $result);
     }
 
     public function testGetFilteredPermission(){
         $model = new UserPermissionModel();
-        $data = ['name' => 'permitest'];
-        $data2 = ['name' => 'test deux permitest'];
-
-        $model->createPermission($data);
-        $model->createPermission($data2);
 
         $result = $model->getFilteredPermission('permitest');
-        $this->assertEquals(2, $result);
+        $this->assertEquals(3, $result);
 
-        $result = $model->getFilteredPermission('deux');
+        $result = $model->getFilteredPermission('test3');
         $this->assertEquals(1, $result);
     }
 
     public function testGetPaginatedPermission() {
         $model = new UserPermissionModel();
-
-        $data = ['name' => 'permitest'];
-        $data2 = ['name' => 'permitest2'];
-        $data3 = ['name' => 'permitest3'];
-
-        $model->createPermission($data);
-        $model->createPermission($data2);
-        $model->createPermission($data3);
 
         $result = $model->getPaginatedPermission(0,3, '', 'name', 'ASC');
         $this->assertIsArray($result);
